ab6135e6129e96b16fb431e260309f3bea025861
Axoncodes <devops@axoncodes.com>
2023-04-16 21:22:34 +0330

    use postgres

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,12 @@
 module.exports = ({ env }) => ({
     seo: {enabled: true},
+    "vercel-deploy": {
+        enabled: true,
+        config: {
+            deployHook: "https://api.vercel.com/v1/integrations/deploy/prj_ovZxM78cJZVgziwZw8EmPkwFPeVG/uPIuO9BjgR",
+            apiToken: "22Sya2FgeU0W909tPcq2OwWP",
+            appFilter: "homapilot-cms",
+            roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+        },
+    },
 });
